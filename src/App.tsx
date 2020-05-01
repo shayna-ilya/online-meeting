a325f3abd71b47b865a7adff957d702a5133891b
ilia <shayger751@gmail.com>
2020-05-01 13:27:18 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AppContainer = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+`;
+
+
+const App = () => {
+
+  return (
+    <AppContainer>
+
+    </AppContainer>
+  );
+};
+
+export default App;
