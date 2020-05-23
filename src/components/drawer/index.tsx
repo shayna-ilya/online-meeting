56c40bc2125b41f2c072c37c5439311066865b42
zausailov <zausailov@reksoft.ru>
2020-05-24 01:25:46 +0300

    #18 set empty fiels after send message

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -78,6 +78,7 @@ export const AppDrawer: React.FC<Props> = ({ isOpen, onClose }) => {
           longitude: newMessageMarkerCoordinates[1],
         }),
       );
+      setMessageFieldValue('');
     }
   }, [dispatch, messageFieldValue, newMessageMarkerCoordinates]);
 
