edadcca2e27f8864e67e1adaf06e48a4bd3c1055
ilia <shayger751@gmail.com>
2020-05-23 23:57:13 +0300

    Added a dollarRate and the Putin components

---
+++ src/components/dollar-rate/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const DollarRate: React.FC = () => {
+  return <p>Курс доллара</p>;
+};
---
+++ src/components/putin/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Putin: React.FC = () => {
+  return <p>Путин у власти уже: </p>;
+};
